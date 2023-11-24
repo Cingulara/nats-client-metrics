a7be7dc957a7f6cc43852b92a37d40a66cd20515
DaleBinghamSoteriaSoft <dale.bingham@soteriasoft.com>
2023-11-24 11:07:52 -0500

    fix outdated references not needed

--- src/Startup.cs
+++ src/Startup.cs
@@ -40,7 +40,7 @@ namespace nats_client_metrics
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             
             app.UseHttpsRedirection();
